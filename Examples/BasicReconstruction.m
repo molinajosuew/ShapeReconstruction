e9da368f0b5cbeceb2216bc2c0526c90c6ad727b
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2018-11-12 17:17:28 -0600

    added bernstein to power coefficients

--- Examples/BasicReconstruction.m
+++ Examples/BasicReconstruction.m
@@ -1,10 +1,12 @@
 clear;
 clc;
 load('Image01.mat');
-J = GetSeparableBernsteinImage(Algorithm(ConvolveAndDownsample(I, 8, 1, 1), 4), 4, 4, size(I, 1), size(I, 2));
+C = Algorithm(ConvolveAndDownsample(I, 8, 1, 1), 4);
+J = GetSeparableBernsteinImage(C, 4, 4, size(I, 1), size(I, 2));
 figure;
 imshow(I);
 figure;
 imshow(J);
 figure;
 imshow(abs(I - J));
+DisplayBernsteinToPowerCoefficients(C);
