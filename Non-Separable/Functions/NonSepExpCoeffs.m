73c589881e1acacc633dea7a7467ef05e667f8d8
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2019-01-02 23:21:08 -0600

    images

--- Non-Separable/Functions/NonSepExpCoeffs.m
+++ Non-Separable/Functions/NonSepExpCoeffs.m
@@ -1,17 +1,14 @@
-% n = degree
-% a & b = resolution
 function NSEC = NonSepExpCoeffs(n, a, b)
-    K2 = DualExpansionCoefficients(n, a);
-    K1 = DualExpansionCoefficients(n, b);
-    NSEC = zeros(n + 1, n + 1, size(K1, 2), size(K2, 2));
-
-    for i = 0 : n
-        for j = 0 : n - i
-            for q = 0 : n - i - j
-                for r = 0 : n - i - j - q
-                    NSEC(1 + i, 1 + j, :, :) = NSEC(1 + i, 1 + j, :, :) + reshape(K2(1 + r + j, :)' * nchoosek(n, i) * nchoosek(n - i, j) * factorial(n - i - j) / (factorial(n - i - j - q - r) * factorial(q) * factorial(r)) * (- 1) ^ (q + r) * K1(1 + q + i, :), 1, 1, size(K1, 2), size(K2, 2));
-                end
+C1 = DualExpansionCoefficients(n, a);
+C2 = DualExpansionCoefficients(n, b);
+NSEC = zeros(n + 1, n + 1, size(C2, 2), size(C1, 2));
+for i = 0 : n
+    for j = 0 : n - i
+        for q = 0 : n - i - j
+            for r = 0 : n - i - j - q
+                NSEC(1 + i, 1 + j, :, :) = NSEC(1 + i, 1 + j, :, :) + reshape(C1(1 + r + j, :)' * nchoosek(n, i) * nchoosek(n - i, j) * factorial(n - i - j) / (factorial(n - i - j - q - r) * factorial(q) * factorial(r)) * (- 1) ^ (q + r) * C2(1 + q + i, :), 1, 1, size(C2, 2), size(C1, 2));
             end
         end
     end
+end
 end
--- Non-Separable/Functions/NonSepMoment.m
+++ Non-Separable/Functions/NonSepMoment.m
@@ -1,8 +1,7 @@
 function NonSepMomentOut = NonSepMoment(I, n, i, j)
 if 0 <= n && 0 <= i && 0 <= j && i + j <= n
     K = NonSepExpCoeffs(n, size(I, 1), size(I, 2));
-    K = reshape(K(1 + i, 1 + j, 1 + floor(n / 2) : end - floor(n / 2), 1 + floor(n / 2) : end - floor(n / 2)), size(I, 1), size(I, 2));
-    NonSepMomentOut = sum(sum(K .* I));
+    NonSepMomentOut = sum(sum(reshape(K(1 + i, 1 + j, 1 + floor(n / 2) : end - floor(n / 2), 1 + floor(n / 2) : end - floor(n / 2)), size(I, 1), size(I, 2)) .* I));
 else
     NonSepMomentOut = 0;
 end
