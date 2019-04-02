2af21c7945f1efdf889693dca73d0f65d82e315c
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2019-04-02 18:44:51 -0500

    fixed a tiny bug when down-sampling

--- Power Basis/Examples/PowerBaseBasicRec.m
+++ Power Basis/Examples/PowerBaseBasicRec.m
@@ -5,7 +5,11 @@ l = 1;
 n = 4;
 load('Image01.mat');
 
-K = PowerBaseAlg(ConvolveAndDownsample(I, n + ceil(n / 2), 1, 1), n, l);
+% Images should have dimensions of the form 2 ^ k + 1, k > 0, to make the down-sampling work.
+I = [I, zeros(size(I, 1), 1)];
+I = [I; zeros(1, size(I, 2))];
+
+K = PowerBaseAlg(ConvolveAndDownsample(I, n + ceil(n / 2), 16, 16), n, l);
 J = PowerBaseImg(K, n, size(I, 1), size(I, 2), l);
 
 figure;
