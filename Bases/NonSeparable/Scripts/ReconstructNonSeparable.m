d51fd7f1e2925d8f23360de104ce548d525b20de
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2019-05-21 19:17:03 -0500

    curation

--- Bases/NonSeparable/Scripts/ReconstructNonSeparable.m
+++ Bases/NonSeparable/Scripts/ReconstructNonSeparable.m
@@ -1,26 +1,15 @@
-% This script loads and reconstructs non-separable Bernstein polynomials.
-
 clear;
 clc;
 
 n = 4;
-L = 100;
-
-m_x = 5;
-m_y = 5;
-
+L = 10;
+m_x = 50;
+m_y = 50;
 psnr = - 1;
 
 x_n = m_x * L + 1;
 y_n = m_y * L + 1;
+I = GetImageOfNonSeparable(GetRandomNonSeparable(n, L), L, x_n, y_n);
+R = GetImageOfNonSeparable(NonSeparableReconstruction(I, n, L, psnr), L, x_n, y_n);
 
-while true
-%     I = GetImageOfPowerPolynomial(GetRandomPowerPolynomial(n, - 5, 5, - 5, 5, false), - 5, 5, - 5, 5, x_n, y_n);
-    I = GetImageOfNonSeparable(GetRandomNonSeparable(n, L), L, x_n, y_n);
-    R = GetImageOfNonSeparable(NonSeparableReconstruction(I, n, L, psnr), L, x_n, y_n);
-    
-    if abs(1 - SorensenDiceCoefficient(I, R)) < 0.01
-        imshow(abs(I - R));
-        break;
-    end
-end
+imshow(abs(I - R));
--- Bases/Power/Scripts/ReconstructPower.m
+++ Bases/Power/Scripts/ReconstructPower.m
@@ -1,35 +1,19 @@
-% This script reconstructs one noisy power polynomial.
+clear;
+clc;
 
 n = 4;
-
-x_a = - 5;
-x_b = 5;
-y_a = - 5;
-y_b = 5;
-
-m_x = 25;
-m_y = 30;
-
+x_a = 0;
+x_b = 10;
+y_a = 0;
+y_b = 10;
+m_x = 50;
+m_y = 50;
 psnr = - 1;
 
 x_n = m_x * (x_b - x_a) + 1;
 y_n = m_y * (y_b - y_a) + 1;
-
-% I = RandomStablyBounded4thDegreeBivariatePolynomial(x_a, x_b, y_a, y_b, x_n, y_n);
-
-P = GetRandomPowerPolynomial(n, x_a, x_b, y_a, y_b, false);
-I = GetImageOfPowerPolynomial(P, x_a, x_b, y_a, y_b, x_n, y_n);
-% figure;
-% imshow(I);
-
-[C, D] = PowerReconstruction(I, n, x_a, x_b, y_a, y_b, psnr);
-
-% figure;
-% imshow(D);
-
-R = GetImageOfPowerPolynomial(C, x_a, x_b, y_a, y_b, x_n, y_n);
-% figure;
-% imshow(R);
+I = GetImageOfPower(GetRandomPower(n, x_a, x_b, y_a, y_b, false), x_a, x_b, y_a, y_b, x_n, y_n);
+R = GetImageOfPower(PowerReconstruction(I, n, x_a, x_b, y_a, y_b, psnr), x_a, x_b, y_a, y_b, x_n, y_n);
 
 figure;
 imshow(abs(I - R));
