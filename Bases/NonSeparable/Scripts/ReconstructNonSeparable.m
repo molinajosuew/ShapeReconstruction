5d627c732b8399a2d3abad6ec03a31eb9d9c2ae4
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2019-05-24 20:26:38 -0500

    poster images pt. 1

--- Bases/NonSeparable/Scripts/ReconstructNonSeparable.m
+++ Bases/NonSeparable/Scripts/ReconstructNonSeparable.m
@@ -1,15 +1,19 @@
 clear;
 clc;
 
-n = 4;
-L = 10;
-m_x = 50;
-m_y = 50;
+n = 2;
+L = 2;
+m_x = 250;
+m_y = 250;
 psnr = - 1;
 
 x_n = m_x * L + 1;
 y_n = m_y * L + 1;
 I = GetImageOfNonSeparable(GetRandomNonSeparable(n, L), L, x_n, y_n);
-R = GetImageOfNonSeparable(NonSeparableReconstruction(I, n, L, psnr), L, x_n, y_n);
+[C, D] = NonSeparableReconstruction(I, n, L, psnr);
+R = GetImageOfNonSeparable(C, L, x_n, y_n);
 
+figure;
+imshow(D);
+figure;
 imshow(abs(I - R));
