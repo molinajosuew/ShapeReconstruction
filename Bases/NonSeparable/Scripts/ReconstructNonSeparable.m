75875931b6387b32d720b02ddc07e3c24b35b07e
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2019-07-29 10:57:23 -0500

    paper data

--- Bases/NonSeparable/Scripts/ReconstructNonSeparable.m
+++ Bases/NonSeparable/Scripts/ReconstructNonSeparable.m
@@ -2,49 +2,15 @@ clear;
 clc;
 
 n = 4;
-L = 2 ^ 4;
-m_x = 2 ^ 5;
-m_y = 2 ^ 5;
+L = 2 ^ 5;
+m_x = 2 ^ 4;
+m_y = 2 ^ 4;
 psnr = - 1;
 
 x_n = m_x * L + 1;
 y_n = m_y * L + 1;
-IC = GetRandomNonSeparable(n, L);
-I = GetImageOfNonSeparable(IC, L, x_n, y_n);
-while true
-    [RC, D] = NonSeparableReconstruction(I, n, L, psnr);
-    R = GetImageOfNonSeparable(RC, L, x_n, y_n);
-    
-    % figure;
-    % imagesc(I);
-    % title('Image');
-    % colorbar;
-    % axis off;
-    %
-    % figure;
-    % imagesc(D);
-    % title('Down-Sample');
-    % colorbar;
-    % axis off;
-    
-    %     figure;
-    %     imagesc(abs(I - R));
-    %     title('Non-Separable Reconstruction 50 dB');
-    %     colorbar;
-    %     axis off;
-    if SorensenDiceCoefficient(I, R) >= .99
-        break;
-    end
-end
 
-% figure;
-% imagesc(D);
-% title('Down-Sample');
-% colorbar;
-% axis off;
+[RC, D] = NonSeparableReconstruction(I, n, L, psnr);
+R = GetImageOfNonSeparable(RC, L, x_n, y_n);
 
-figure;
-imagesc(abs(I - R));
-title('Non-Separable Reconstruction 30 dB');
-colorbar;
-axis off;
+imshow(abs(I - R));
--- Bases/Power/Scripts/ReconstructPower.m
+++ Bases/Power/Scripts/ReconstructPower.m
@@ -2,21 +2,18 @@ clear;
 clc;
 
 n = 4;
-x_a = - 5;
-x_b = 5;
-y_a = - 5;
-y_b = 5;
-m_x = 2 ^ 5;
-m_y = 2 ^ 5;
+x_a = - 1;
+x_b = 1;
+y_a = - 1;
+y_b = 1;
+m_x = 2 ^ 8;
+m_y = 2 ^ 8;
 psnr = - 1;
 
 x_n = m_x * (x_b - x_a) + 1;
 y_n = m_y * (y_b - y_a) + 1;
+
 I = GetImageOfPower(GetRandomPower(n, x_a, x_b, y_a, y_b, false), x_a, x_b, y_a, y_b, x_n, y_n);
 R = GetImageOfPower(PowerReconstruction(I, n, x_a, x_b, y_a, y_b, psnr), x_a, x_b, y_a, y_b, x_n, y_n);
 
-figure;
-imagesc(abs(I - R));
-title('Power Reconstruction 30 dB');
-colorbar;
-axis off;
+imshow(abs(I - R));
