5ba0275c5fd06411a460cb8a8cf485b9d8d60e3f
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2019-10-14 23:23:20 -0500

    updated fig 3

--- Bases/Power/Scripts/ReconstructPower.m
+++ Bases/Power/Scripts/ReconstructPower.m
@@ -1,20 +1,20 @@
-%clear;
-%clc;
+% clear;
+% clc;
 
 n = 4;
-x_a = - 1;
-x_b = 1;
-y_a = - 1;
-y_b = 1;
-m_x = 2 ^ 8;
-m_y = 2 ^ 8;
-psnr = - 1;
+x_a = - 5;
+x_b = 5;
+y_a = - 5;
+y_b = 5;
+m_x = 2 ^ 6;
+m_y = 2 ^ 6;
+psnr = 80;
 
 x_n = m_x * (x_b - x_a) + 1;
 y_n = m_y * (y_b - y_a) + 1;
 
-%I = GetImageOfPower(GetRandomPower(n, x_a, x_b, y_a, y_b, false), x_a, x_b, y_a, y_b, x_n, y_n);
-[C, D] = PowerReconstruction(I, n, x_a, x_b, y_a, y_b, psnr);
-R = GetImageOfPower(C, x_a, x_b, y_a, y_b, x_n, y_n);
+I = GetImageOfPower(IC, - 1, 1, - 1, 1, x_n, y_n);
 
-imshow(abs(I - ~ R));
+[V_best, ~, ~] = VetterliReconstruction(I, x_a, x_b, y_a, y_b, x_n, y_n, psnr);
+
+imshow(abs(I - V_best));
