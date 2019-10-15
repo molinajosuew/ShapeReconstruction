b7e9b82d66d34002ba33ba166393bb02a947246a
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2019-10-14 23:24:20 -0500

    additional kernels

--- Bases/Power/Scripts/ReconstructPower.m
+++ Bases/Power/Scripts/ReconstructPower.m
@@ -1,19 +1,19 @@
-%clear;
-%clc;
+% clear;
+% clc;
 
 n = 4;
-x_a = - 1;
-x_b = 1;
-y_a = - 1;
-y_b = 1;
-m_x = 2 ^ 8;
-m_y = 2 ^ 8;
+x_a = - 16;
+x_b = 16;
+y_a = - 16;
+y_b = 16;
+m_x = 2 ^ 4;
+m_y = 2 ^ 4;
 psnr = - 1;
 
 x_n = m_x * (x_b - x_a) + 1;
 y_n = m_y * (y_b - y_a) + 1;
 
-%I = GetImageOfPower(GetRandomPower(n, x_a, x_b, y_a, y_b, false), x_a, x_b, y_a, y_b, x_n, y_n);
+% I = GetImageOfPower(IC, x_a, x_b, y_a, y_b, x_n, y_n);
 [C, D] = PowerReconstruction(I, n, x_a, x_b, y_a, y_b, psnr);
 R = GetImageOfPower(C, x_a, x_b, y_a, y_b, x_n, y_n);
 
--- Laboratory/Scripts/PlotStatistics.m
+++ Laboratory/Scripts/PlotStatistics.m
@@ -1,3 +1,1 @@
-boxplot(X, 'Labels', {'Non-Separable Bernstein', 'Generalized Moments', 'Enhanced Generalized Moments'}, 'Colors', 'k', 'Whisker', 1000000000);
-title('Reconstruction Comparison of 100 Images with a PSNR of 50 dB');
-ylabel('Sørensen–Dice Coefficient');
+boxplot(D(:, 2 : 2), 'Colors', 'k', 'Whisker', 1000000000);
